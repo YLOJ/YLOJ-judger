fd424e01ee74f01b46947c8b6cb71673cad4aeeb
QAQAutoMaton <lk@qaq-am.com>
2019-12-17 21:02:54 +0800

    加入env.py的样例env.example.py

---
+++ env.example.py
@@ -0,0 +1,13 @@
+pathOfSandbox="./sandbox"
+host="localhost"
+user="root"
+password="password"
+database="database"
+redishost='localhost'
+redisport=6379
+redispassword='password'
+submission_update_token="token"
+update_link="http://localhost"
+cgroup_name="cgroup1"
+data_path="/home/lk/YLOJ/storage/app/data"
+
